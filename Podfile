23e7412a93fb6e1e2ca71c456cf9134cc603ea45
Jacky Huang <jackyhuang@Jackyde-MacBook-Pro.local>
2018-05-06 17:36:51 +0800

    Rebuild the Project

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '10.0'
+
+target 'RunRRR 2.0' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'GoogleMaps'
+  pod 'SnapKit'
+  pod 'Alamofire', '~> 4.7'
+  pod 'SwiftyJSON', '~> 4.0'  
+# Pods for RunRRR 2.0
+
+  target 'RunRRR 2.0Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RunRRR 2.0UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ RunRRR 2.0/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RunRRR 2.0
+//
+//  Created by Jacky Huang on 2018/4/22.
+//  Copyright © 2018年 ColdeGarage. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.red
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
